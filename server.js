4ed03e52d7a8823520931acd8634e66853723a04
Junyoung Ho <holee1213@naver.com>
2024-01-04 18:38:02 +0900

    feat : link server review data

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from "cors";
 import imgUploadRouter from "./router/imgUploadRouter.js";
+import reviewRouter from "./router/reviewRouter.js";
+import path from 'path';
 
 const PORT = 8000;
 const server = express();
 
 server.use(cors()).use(express.json()).use(express.urlencoded());
 
-server.use("/imgupload", imgUploadRouter)
+//router
 
+server.use("/imgupload", imgUploadRouter)
+server.use("/review",reviewRouter)
+//get server file
 server.use('/imgupload',express.static(path.join('imgUploads')))
 
 server.listen(PORT, () => {
